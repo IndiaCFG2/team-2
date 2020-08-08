2f75fa92f805659f27c9a5785527d97b71eb67ee
Avinash <avinashagarwal14@gmail.com>
2020-08-08 21:04:25 +0530

    add donor new post

---
+++ share-for-good/src/Components/Dashboards/DonorPostsNew.js
@@ -0,0 +1,51 @@
+import React, { Component } from 'react';
+import db from '../firebase';
+import { render } from '@testing-library/react';
+
+class DonorPosts extends Component {
+    
+    constructor(props) {
+        super(props);
+        this.state = {
+            user: '',
+            donor_post:{
+                items: [],
+                description: '',
+                donor_id: ''
+            }
+        }
+    }
+
+    onSubmit () {
+        db.collection('donor_posts')
+        .add({
+            donor_id: this.state.user,
+            description: this.state.donor_post.description,
+        }).then(function(docRef) {
+            this.state.items.map((item) => {
+                db.collection('donor_posts').doc(docRef.id).collection('items')
+                .add({
+                    description: item.description,
+                    img_url: item.img_url,
+                    video_url: item.video_url,
+                    title: item.title,
+                    video_url: item.video_url
+                })
+            })
+        })
+        .catch(function(error) {
+            console.error("Error adding Donor Post: ", error);
+        });;
+    }
+
+    render() {
+        return (
+            <div>
+                Create Form and Save Data!
+            </div>
+        );
+    }
+
+}
+
+export default DonorPosts;
---
+++ share-for-good/src/Components/Dashboards/NGOPostsNew.js
@@ -0,0 +1,50 @@
+import React, { Component } from 'react';
+import db from '../firebase';
+import { render } from '@testing-library/react';
+
+class NGOPosts extends Component {
+    
+    constructor(props) {
+        super(props);
+        this.state = {
+            user: '',
+            ngo_post:{
+                items: [],
+                description: '',
+                donor_id: ''
+            }
+        }
+    }
+
+    onSubmit () {
+        db.collection('ngo_posts')
+        .add({
+            ngo_id: this.state.user,
+            description: this.state.ngo_post.description,
+        }).then(function(docRef) {
+            this.state.items.map((item) => {
+                db.collection('ngo_posts').doc(docRef.id).collection('items')
+                .add({
+                    description: item.description,
+                    img_url: item.img_url,
+                    video_url: item.video_url,
+                    title: item.title,
+                    video_url: item.video_url
+                })
+            })
+        })
+        .catch(function(error) {
+            console.error("Error adding NGO Post: ", error);
+        });;
+    }
+
+    render() {
+        return (
+            <div>
+                Create Form and Save Data!
+            </div>
+        );
+    }
+}
+
+export default NGOPosts;
