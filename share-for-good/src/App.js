8fc51d53245fd45ca582361a15b1441c615533fe
Avinash <avinashagarwal14@gmail.com>
2020-08-08 15:59:50 +0530

    profile added

--- share-for-good/src/App.js
+++ share-for-good/src/App.js
@@ -3,7 +3,7 @@ import {Route, Switch } from 'react-router-dom';
 import Home from './Components/Home/Home';
 import Auth from './Components/Auth/Auth';
 import Chat from './Components/Chat/Chat';
-
+import Profile from './Components/Profile/Profile';
 
 function App() {
     return (
@@ -11,6 +11,7 @@ function App() {
             <Switch>
                 <Route path="/" component={Home} exact />
                 <Route path="/auth" component={Auth} />
+                <Route path="/profile" component={Profile} />
                 <Route path="/chat" component={Chat} />
             </Switch>
         </main>
---
+++ share-for-good/src/Components/Profile/Profile.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import { render } from '@testing-library/react';
+
+class Profile extends Component {
+    render() {
+        return (
+            <div>
+                Hello from Profile!
+            </div>
+        );
+    }
+}
+
+export default Profile;
