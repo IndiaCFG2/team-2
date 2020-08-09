428e7e14bed53bd117d53525924cc6d0896e4e62
Avinash <avinashagarwal14@gmail.com>
2020-08-09 07:11:04 +0530

    readme updated

--- share-for-good/src/Components/Dashboards/NGOPosts.js
+++ share-for-good/src/Components/Dashboards/NGOPosts.js
@@ -40,9 +40,9 @@ class NGOPosts extends Component {
                     {
                     posts.map((post) => {
                     return (
-                        <div class="col-md-4">
+                        <div class="col-md-4 p-1">
                         <div class="card" style={{ width: "18rem" }}>
-                            <img class="card-img-top" src={post.img_url} style={{ width: "100px", height: "100px" }}
+                            <img class="card-img-top mx-auto" src={post.img_url} style={{ width: "100px", height: "100px" }}
                                 alt="Card image cap" />
                             <div class="card-body">
                                 <h5 class="card-title">{post.title}</h5>
